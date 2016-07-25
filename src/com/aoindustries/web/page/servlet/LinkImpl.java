898a703c2d3dd06c90e269b6a39b9dadd6b2ac4d
Dan Armstrong <dan@aoindustries.com>
2016-07-25 21:29:16 +0000

    Splitting non-km parts into more general purpose taglib..

---
+++ src/com/aoindustries/web/page/servlet/LinkImpl.java
@@ -0,0 +1,69 @@
+/*
+ * ao-web-page-servlet - Java API for modeling web page content and relationships in a Servlet environment.
+ * Copyright (C) 2013, 2014, 2015, 2016  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-web-page-servlet.
+ *
+ * ao-web-page-servlet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-web-page-servlet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-web-page-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.web.page.servlet;
+
+import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.encodeTextInXhtmlAttribute;
+import static com.aoindustries.encoding.TextInXhtmlEncoder.encodeTextInXhtml;
+import com.aoindustries.web.page.PageRef;
+import java.io.IOException;
+import java.io.Writer;
+
+final public class LinkImpl {
+
+	/**
+	 * Writes a broken path reference as "¿/book/path{#targetId}?", encoding for XHTML.
+	 */
+	public static void writeBrokenPathInXhtml(PageRef pageRef, String targetId, Writer out) throws IOException {
+		out.write("¿");
+		encodeTextInXhtml(pageRef.getBookPrefix(), out);
+		encodeTextInXhtml(pageRef.getPath(), out);
+		if(targetId != null) {
+			encodeTextInXhtml('#', out);
+			encodeTextInXhtml(targetId, out);
+		}
+		out.write('?');
+	}
+
+	/**
+	 * Writes a broken path reference as "¿/book/path?", encoding for XHTML.
+	 */
+	public static void writeBrokenPathInXhtml(PageRef pageRef, Writer out) throws IOException {
+		writeBrokenPathInXhtml(pageRef, null, out);
+	}
+
+	/**
+	 * Writes a broken path reference as "¿/book/path?", encoding for XML attribute.
+	 */
+	public static void writeBrokenPathInXhtmlAttribute(PageRef pageRef, Writer out) throws IOException {
+		out.write("¿");
+		encodeTextInXhtmlAttribute(pageRef.getBookPrefix(), out);
+		encodeTextInXhtmlAttribute(pageRef.getPath(), out);
+		out.write('?');
+	}
+
+	/**
+	 * Make no instances.
+	 */
+	private LinkImpl() {
+	}
+}
