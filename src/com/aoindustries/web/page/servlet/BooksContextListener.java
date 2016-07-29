24568811d0907ce8245339d295d9411fc05cc61d
Dan Armstrong <dan@aoindustries.com>
2016-07-29 21:38:40 +0000

    Books now contain references to their parent pages.

--- src/com/aoindustries/web/page/servlet/BooksContextListener.java
+++ src/com/aoindustries/web/page/servlet/BooksContextListener.java
@@ -28,63 +28,98 @@ import com.aoindustries.util.PropertiesUtils;
 import com.aoindustries.util.StringUtility;
 import com.aoindustries.util.WrappedException;
 import com.aoindustries.web.page.Book;
+import com.aoindustries.web.page.PageRef;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletRequest;
 
-@WebListener("Exposes all the configured Books as an application-scope map \"books\".")
+@WebListener(
+	"Exposes all the configured books as an application-scope Map<String,Book> \"" + BooksContextListener.BOOKS_ATTRIBUTE_NAME + "\".\n"
+	+ "Exposes all the configured missing book names as an application-scope Set<String> \"" + BooksContextListener.MISSING_BOOKS_ATTRIBUTE_NAME+ "\".\n"
+	+ "Exposes the configured root book as an application-scope Book \"" + BooksContextListener.ROOT_BOOK_ATTRIBUTE_NAME+ "\"."
+)
 public class BooksContextListener implements ServletContextListener {
 
-	private static final String BOOKS_ATTRIBUTE_NAME = "books";
-	private static final String MISSING_BOOKS_ATTRIBUTE_NAME = "missingBooks";
-	private static final String ROOT_BOOK = "rootBook";
+	static final String BOOKS_ATTRIBUTE_NAME = "books";
+	static final String MISSING_BOOKS_ATTRIBUTE_NAME = "missingBooks";
+	static final String ROOT_BOOK_ATTRIBUTE_NAME = "rootBook";
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		try {
 			ServletContext servletContext = event.getServletContext();
-			// Load books
-			if(servletContext.getAttribute(BOOKS_ATTRIBUTE_NAME)!=null) throw new IllegalStateException("Application-scope attribute already present: " + BOOKS_ATTRIBUTE_NAME);
-			Map<String,Book> books = new LinkedHashMap<>();
-			for(String name : StringUtility.splitStringCommaSpace(servletContext.getInitParameter("books"))) {
+			// Load missingBooks
+			Set<String> missingBooks = new LinkedHashSet<>();
+			for(String name : StringUtility.splitStringCommaSpace(servletContext.getInitParameter(MISSING_BOOKS_ATTRIBUTE_NAME))) {
 				if(!name.isEmpty()) {
+					missingBooks.add(name);
+				}
+			}
+			// Load books
+			String rootBookName = servletContext.getInitParameter(ROOT_BOOK_ATTRIBUTE_NAME);
+			if(rootBookName == null || rootBookName.isEmpty()) throw new IllegalStateException('"' + ROOT_BOOK_ATTRIBUTE_NAME + "\" not provided");
+			Map<String,Book> books = new LinkedHashMap<>();
+			for(String name : StringUtility.splitStringCommaSpace(servletContext.getInitParameter(BOOKS_ATTRIBUTE_NAME))) {
+				if(!name.isEmpty()) {
+					if(missingBooks.contains(name)) throw new IllegalStateException("Book also listed in \"" + MISSING_BOOKS_ATTRIBUTE_NAME + "\": " + name);
 					String cvsworkDirectoryAttribute = "book." + name + ".cvsworkDirectory";
 					String cvsworkDirectory = servletContext.getInitParameter(cvsworkDirectoryAttribute);
 					if(cvsworkDirectory == null) throw new IllegalStateException("Required context parameter not present: " + cvsworkDirectoryAttribute);
+					List<PageRef> parentPages = new ArrayList<>();
+					for(int i=1; i<Integer.MAX_VALUE; i++) {
+						String parentBookNameAttribute = "book." + name + ".parent." + i + ".book";
+						String parentBookName = servletContext.getInitParameter(parentBookNameAttribute);
+						String parentPageAttribute = "book." + name + ".parent." + i + ".page";
+						String parentPage = servletContext.getInitParameter(parentPageAttribute);
+						// Stop on the first not found
+						if(parentBookName == null && parentPage == null) break;
+						if(parentBookName == null) throw new IllegalArgumentException("parent book required when parent page provided: " + parentPageAttribute + " = " + parentPage);
+						if(parentPage == null) throw new IllegalArgumentException("parent page required when parent book provided: " + parentBookNameAttribute + " = " + parentBookName);
+						if(missingBooks.contains(parentBookName)) throw new IllegalStateException("parent book may not be a \"" + MISSING_BOOKS_ATTRIBUTE_NAME + "\": " + parentBookNameAttribute + " = " + parentBookName);
+						Book parentBook = books.get(parentBookName);
+						if(parentBook == null) throw new IllegalStateException("parent book not found (loading order currently matters): " + parentBookNameAttribute + " = " + parentBookName);
+						parentPages.add(new PageRef(parentBook, parentPage));
+					}
+					if(name.equals(rootBookName)) {
+						if(!parentPages.isEmpty()) {
+							throw new IllegalStateException('"' + ROOT_BOOK_ATTRIBUTE_NAME + "\" may not have any parents: " + rootBookName);
+						}
+					} else {
+						if(parentPages.isEmpty()) {
+							throw new IllegalStateException("Non-root books must have at least one parent: " + name);
+						}
+					}
 					books.put(
 						name,
 						new Book(
 							name,
 							cvsworkDirectory,
+							parentPages,
 							PropertiesUtils.loadFromResource(servletContext, ("/".equals(name) ? "" : name) + "/book.properties")
 						)
 					);
 				}
 			}
-			servletContext.setAttribute(
-				BOOKS_ATTRIBUTE_NAME,
-				AoCollections.optimalUnmodifiableMap(books)
-			);
-			// Load missingBooks
+			// Load rootBook
+			if(missingBooks.contains(rootBookName)) throw new IllegalStateException('"' + ROOT_BOOK_ATTRIBUTE_NAME + "\" may not be a \"" + MISSING_BOOKS_ATTRIBUTE_NAME + "\": " + rootBookName);
+			Book rootBook = books.get(rootBookName);
+			if(rootBook == null) throw new IllegalStateException('"' + ROOT_BOOK_ATTRIBUTE_NAME + "\" is not found in \"" + BOOKS_ATTRIBUTE_NAME + "\": " + rootBookName);
+			// Successful startup
+			if(servletContext.getAttribute(BOOKS_ATTRIBUTE_NAME)!=null) throw new IllegalStateException("Application-scope attribute already present: " + BOOKS_ATTRIBUTE_NAME);
+			servletContext.setAttribute(BOOKS_ATTRIBUTE_NAME, AoCollections.optimalUnmodifiableMap(books));
 			if(servletContext.getAttribute(MISSING_BOOKS_ATTRIBUTE_NAME)!=null) throw new IllegalStateException("Application-scope attribute already present: " + MISSING_BOOKS_ATTRIBUTE_NAME);
-			Set<String> missingBooks = new LinkedHashSet<>();
-			for(String name : StringUtility.splitStringCommaSpace(servletContext.getInitParameter("missingBooks"))) {
-				if(!name.isEmpty()) {
-					missingBooks.add(name);
-				}
-			}
-			servletContext.setAttribute(
-				MISSING_BOOKS_ATTRIBUTE_NAME,
-				AoCollections.optimalUnmodifiableSet(missingBooks)
-			);
+			servletContext.setAttribute(MISSING_BOOKS_ATTRIBUTE_NAME, AoCollections.optimalUnmodifiableSet(missingBooks));
+			if(servletContext.getAttribute(ROOT_BOOK_ATTRIBUTE_NAME)!=null) throw new IllegalStateException("Application-scope attribute already present: " + ROOT_BOOK_ATTRIBUTE_NAME);
+			servletContext.setAttribute(ROOT_BOOK_ATTRIBUTE_NAME, rootBook);
 		} catch(IOException e) {
 			throw new WrappedException(e);
 		}
@@ -102,6 +137,15 @@ public class BooksContextListener implements ServletContextListener {
 		Set<String> missingBooks = (Set)servletContext.getAttribute(MISSING_BOOKS_ATTRIBUTE_NAME);
 		if(missingBooks == null) throw new IllegalStateException("Application-scope attribute not found: " + MISSING_BOOKS_ATTRIBUTE_NAME);
 		return missingBooks;
+	}
+
+	/**
+	 * Gets the root book as configured in web.xml
+	 */
+	public static Book getRootBook(ServletContext servletContext) throws IllegalStateException {
+		Book rootBook = (Book)servletContext.getAttribute(ROOT_BOOK_ATTRIBUTE_NAME);
+		if(rootBook == null) throw new IllegalStateException("Application-scope attribute not found: " + ROOT_BOOK_ATTRIBUTE_NAME);
+		return rootBook;
 	}
 
 	/**
@@ -132,16 +176,6 @@ public class BooksContextListener implements ServletContextListener {
 	 */
 	public static Book getBook(ServletContext servletContext, HttpServletRequest request) {
 		return getBook(servletContext, Dispatcher.getCurrentPagePath(request));
-	}
-
-	/**
-	 * Gets the root book as configured in web.xml
-	 */
-	public static Book getRootBook(ServletContext servletContext) throws ServletException {
-		String rootBookName = servletContext.getInitParameter(ROOT_BOOK);
-		Book rootBook = BooksContextListener.getBooks(servletContext).get(rootBookName);
-		if(rootBook == null) throw new ServletException("Root book not found: " + rootBookName);
-		return rootBook;
 	}
 
 	@Override
