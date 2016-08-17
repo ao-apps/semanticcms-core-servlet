e45d337503f75452bb554804f7a76d444dadc5d2
AO Industries, Inc. <support@aoindustries.com>
2016-08-17 14:40:52 -0500

    Using auto temp file wrapper in all places.

--- src/main/java/com/semanticcms/core/servlet/Page.java
+++ src/main/java/com/semanticcms/core/servlet/Page.java
@@ -171,20 +171,19 @@ public class Page {
 							);
 							return EmptyResult.getInstance();
 						} else {
-							BufferWriter capturedOut = new SegmentedWriter();
-							try {
-								// Enable temp files if temp file context active
-								capturedOut = TempFileContext.wrapTempFileList(
-									capturedOut,
-									request,
-									// Java 1.8: AutoTempFileWriter::new
-									new TempFileContext.Wrapper<BufferWriter>() {
-										@Override
-										public BufferWriter call(BufferWriter original, TempFileList tempFileList) {
-											return new AutoTempFileWriter(original, tempFileList);
-										}
+							// Enable temp files if temp file context active
+							BufferWriter capturedOut = TempFileContext.wrapTempFileList(
+								new SegmentedWriter(),
+								request,
+								// Java 1.8: AutoTempFileWriter::new
+								new TempFileContext.Wrapper<BufferWriter>() {
+									@Override
+									public BufferWriter call(BufferWriter original, TempFileList tempFileList) {
+										return new AutoTempFileWriter(original, tempFileList);
 									}
-								);
+								}
+							);
+							try {
 								final PrintWriter capturedPW = new PrintWriter(capturedOut);
 								try {
 									final HttpServletResponse newResponse = new HttpServletResponseWrapper(response) {
