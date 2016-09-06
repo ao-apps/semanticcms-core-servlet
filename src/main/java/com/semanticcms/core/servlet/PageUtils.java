497f6e5a951d15fd3792954da509004fe4481f1b
AO Industries, Inc <support@aoindustries.com>
2016-09-06 16:16:08 -0500

    Now supporting per-page allowRobots setting.

--- src/main/java/com/semanticcms/core/servlet/PageUtils.java
+++ src/main/java/com/semanticcms/core/servlet/PageUtils.java
@@ -22,11 +22,14 @@
  */
 package com.semanticcms.core.servlet;
 
+import com.semanticcms.core.model.Book;
 import com.semanticcms.core.model.Element;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.model.PageRef;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -104,6 +107,81 @@ final public class PageUtils {
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * <p>
+	 * Finds the allowRobots setting for the given page.
+	 * </p>
+	 * <p>
+	 * When no allowRobots provided, will use the allowRobots(s) of any parent
+	 * page that is within the same book.  If there are no parent pages
+	 * in this same book, uses the book's allowRobots.
+	 * </p>
+	 * <p>
+	 * When inheriting allowRobots from multiple parent pages, the allowRobots must
+	 * be in exact agreement.  This means exactly the same order and all
+	 * values matching precisely.  Any mismatch in allowRobots will result in
+	 * an exception.
+	 * </p>
+	 */
+	public static boolean findAllowRobots(
+		ServletContext servletContext,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		com.semanticcms.core.model.Page page
+	) throws ServletException, IOException {
+		return findAllowRobotsRecursive(
+			servletContext,
+			request,
+			response,
+			page,
+			new HashMap<PageRef,Boolean>()
+		);
+	}
+
+	private static boolean findAllowRobotsRecursive(
+		ServletContext servletContext,
+		HttpServletRequest request,
+		HttpServletResponse response,
+		com.semanticcms.core.model.Page page,
+		Map<PageRef,Boolean> finished
+	) throws ServletException, IOException {
+		PageRef pageRef = page.getPageRef();
+		assert !finished.containsKey(pageRef);
+		// Use directly set allowRobots first
+		Boolean pageAllowRobots = page.getAllowRobots();
+		if(pageAllowRobots == null) {
+			// Use the allowRobots of all parents in the same book
+			Book book = pageRef.getBook();
+			for(PageRef parentRef : page.getParentPages()) {
+				if(book.equals(parentRef.getBook())) {
+					// Check finished already
+					Boolean parentAllowRobots = finished.get(parentRef);
+					if(parentAllowRobots == null) {
+						// Capture parent and find its allowRobots
+						parentAllowRobots = findAllowRobotsRecursive(
+							servletContext,
+							request,
+							response,
+							CapturePage.capturePage(servletContext, request, response, parentRef, CaptureLevel.PAGE),
+							finished
+						);
+					}
+					if(pageAllowRobots == null) {
+						pageAllowRobots = parentAllowRobots;
+					} else {
+						// Must precisely match when have multiple parents
+						if(!pageAllowRobots.equals(parentAllowRobots)) throw new ServletException("Mismatched allowRobots inherited from different parents: " + pageAllowRobots + " does not match " + parentAllowRobots);
+					}
+				}
+			}
+			// No parents in the same book, use book allowRobots
+			if(pageAllowRobots == null) pageAllowRobots = book.getAllowRobots();
+		}
+		// Store in finished
+		finished.put(pageRef, pageAllowRobots);
+		return pageAllowRobots;
 	}
 
 	/**
