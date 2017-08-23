c2fa35460db22c36596d1c169aaef7b004b90a4f
AO Industries, Inc <support@aoindustries.com>
2017-08-23 16:25:43 -0500

    CurrentCaptureLevel moved to new semanticcms-core-pages-local module

--- src/main/java/com/semanticcms/core/servlet/Element.java
+++ src/main/java/com/semanticcms/core/servlet/Element.java
@@ -32,6 +32,7 @@ import com.semanticcms.core.model.Node;
 import com.semanticcms.core.model.NodeBodyWriter;
 import com.semanticcms.core.model.Page;
 import com.semanticcms.core.pages.CaptureLevel;
+import com.semanticcms.core.pages.local.CurrentCaptureLevel;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletContext;
