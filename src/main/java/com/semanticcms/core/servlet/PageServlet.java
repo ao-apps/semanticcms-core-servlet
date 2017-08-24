932d4ee6f7e8e6da69462f32019c28051f3d6026
AO Industries, Inc. <support@aoindustries.com>
2017-08-23 23:39:30 -0500

    New constant SKIP_PAGE_EXCEPTION.

--- src/main/java/com/semanticcms/core/servlet/PageServlet.java
+++ src/main/java/com/semanticcms/core/servlet/PageServlet.java
@@ -135,7 +135,7 @@ abstract public class PageServlet extends HttpServlet {
 	 */
 	protected void doGet(Page page) throws ServletException, IOException, SkipPageException {
 		Includer.sendError(PageContext.getRequest(), PageContext.getResponse(), HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-		throw new SkipPageException();
+		throw ServletUtil.SKIP_PAGE_EXCEPTION;
 	}
 
 	@Override
@@ -160,7 +160,7 @@ abstract public class PageServlet extends HttpServlet {
 	 */
 	protected void doPost(Page page) throws ServletException, IOException, SkipPageException {
 		Includer.sendError(PageContext.getRequest(), PageContext.getResponse(), HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-		throw new SkipPageException();
+		throw ServletUtil.SKIP_PAGE_EXCEPTION;
 	}
 
 	@Override
@@ -185,7 +185,7 @@ abstract public class PageServlet extends HttpServlet {
 	 */
 	protected void doPut(Page page) throws ServletException, IOException, SkipPageException {
 		Includer.sendError(PageContext.getRequest(), PageContext.getResponse(), HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-		throw new SkipPageException();
+		throw ServletUtil.SKIP_PAGE_EXCEPTION;
 	}
 
 	@Override
@@ -210,7 +210,7 @@ abstract public class PageServlet extends HttpServlet {
 	 */
 	protected void doDelete(Page page) throws ServletException, IOException, SkipPageException {
 		Includer.sendError(PageContext.getRequest(), PageContext.getResponse(), HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-		throw new SkipPageException();
+		throw ServletUtil.SKIP_PAGE_EXCEPTION;
 	}
 
 	@Override
