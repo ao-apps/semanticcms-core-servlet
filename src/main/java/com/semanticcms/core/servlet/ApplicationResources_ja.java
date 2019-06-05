3817403865b6eef89dbdd407a3543b50c13ef52c
AO Industries, Inc <support@aoindustries.com>
2019-06-05 17:14:55 -0500

    Minimized diff versus 1.x branch

--- src/main/java/com/semanticcms/core/servlet/ApplicationResources_ja.java
+++ src/main/java/com/semanticcms/core/servlet/ApplicationResources_ja.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-servlet - Java API for modeling web page content and relationships in a Servlet environment.
- * Copyright (C) 2017  AO Industries, Inc.
+ * Copyright (C) 2017, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
