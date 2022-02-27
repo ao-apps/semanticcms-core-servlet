fea104d51de3ddf614297e094753ec72e6531afc
AO Industries, Inc <support@aoindustries.com>
2022-02-27 00:47:57 -0600

    Using final instead of abstract for static utility classes
    NetBeans 12.6 is warning "Constructor is never used" when abstract, and
    this cannot be suppressed with `@SuppressWarnings("unused")`.

--- src/main/java/com/semanticcms/core/servlet/Resources.java
+++ src/main/java/com/semanticcms/core/servlet/Resources.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-servlet - Java API for modeling web page content and relationships in a Servlet environment.
- * Copyright (C) 2017, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2017, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -27,7 +27,7 @@ import java.util.ResourceBundle;
 /**
  * Provides a simplified interface for obtaining localized values from the ApplicationResources.properties files.
  */
-public abstract class Resources {
+public final class Resources {
 
 	/** Make no instances. */
 	private Resources() {throw new AssertionError();}
