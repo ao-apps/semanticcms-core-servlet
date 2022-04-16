f137e9159b5941870c2438468ac7e382192b7162
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:23 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/core/servlet/SynchronizedCache.java
+++ src/main/java/com/semanticcms/core/servlet/SynchronizedCache.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-servlet - Java API for modeling web page content and relationships in a Servlet environment.
- * Copyright (C) 2016, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.servlet;
 
 import com.aoapps.collections.AoCollections;
