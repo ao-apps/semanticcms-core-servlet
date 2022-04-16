b5734d4adc9df23bd1bfb4e056fd846e0d955bf2
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:14 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- devel/src/main/java/com/semanticcms/core/servlet/i18n/ApplicationResources.java
+++ devel/src/main/java/com/semanticcms/core/servlet/i18n/ApplicationResources.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-core-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.core.servlet.i18n;
 
 import com.aoapps.hodgepodge.i18n.EditableResourceBundle;
