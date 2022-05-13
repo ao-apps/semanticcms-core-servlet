ddf29821bb4c463dbd93bb4b512a3c4d0010b39d
AO Industries, Inc <support@aoindustries.com>
2022-05-13 14:38:31 -0500

    Removed workaround for rewrite-maven-plugin-4.22.2 truncation issue
    We are not longer routinely performing rewrites during development and CI builds

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -44,4 +44,4 @@ module com.semanticcms.core.servlet {
   requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
   requires com.semanticcms.core.pages; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-pages</artifactId>
   requires com.semanticcms.core.pages.local; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-pages-local</artifactId>
-} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
+}
