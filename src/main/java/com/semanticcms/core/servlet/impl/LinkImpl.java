d933b95f15317ae7139742186c9d863990cd45c8
AO Industries, Inc <support@aoindustries.com>
2023-10-02 15:58:16 +0000

    resolveValue moved from ao-taglib to ao-servlet-util

--- src/main/java/com/semanticcms/core/servlet/impl/LinkImpl.java
+++ src/main/java/com/semanticcms/core/servlet/impl/LinkImpl.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-servlet - Java API for modeling web page content and relationships in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -26,7 +26,7 @@ package com.semanticcms.core.servlet.impl;
 import static com.aoapps.encoding.TextInXhtmlAttributeEncoder.encodeTextInXhtmlAttribute;
 import static com.aoapps.encoding.TextInXhtmlEncoder.encodeTextInXhtml;
 import static com.aoapps.lang.Strings.nullIfEmpty;
-import static com.aoapps.taglib.AttributeUtils.resolveValue;
+import static com.aoapps.servlet.el.ElUtils.resolveValue;
 
 import com.aoapps.html.any.AnyA;
 import com.aoapps.html.any.AnyA_c;
